feb3ac846b35e33226b0a989a16d2b53a34a256d
Victor Henrique Mointeiro Lima <victorhenriqu3@outlook.com>
2020-12-31 01:44:29 -0400

    Projeto Base

---
+++ src/Pages/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from "../../Components/Menu"
+import Footer from "../../Components/Footer"
+import Card from '../../Components/Card';
+
+function HomePage() {
+  return (
+    <div>
+    <Menu />
+    <Card/>
+    <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/Pages/Register/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Menu from "../../Components/Menu"
+import Footer from "../../Components/Footer"
+import Forms from '../../Components/Forms';
+
+
+function Register() {
+  return (
+  <div>
+   <Menu/>
+    <Forms/>
+    <Footer/>
+  </div>
+
+  );
+}
+
+export default Register;
