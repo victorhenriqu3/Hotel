e3cc212f7efd3b28ec7b75069e6d5038c28b6178
Victor Henrique <victorhenriqu3@outlook.com>
2023-02-04 16:14:27 -0400

    🎨 feat: add ChakraUI

---
+++ src/style/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	colors: {
+		background: '#EEEEEE',
+		backgroundCard: '#f9f9f9',
+		yellowish: '#FFF159',
+		blueGraysh: {
+			200: '#3345CC',
+			400: '#2F387B',
+		},
+		grey: '#2C2C2C',
+	},
+});
